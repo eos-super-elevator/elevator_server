b195682a85501db92cc06724747d5b2b7cd97720
Alexandre Rousseau <contact@rousseau-alexandre.Fr>
2018-12-18 11:08:35 +0100

    first try

--- index.js
+++ index.js
@@ -2,6 +2,32 @@ const express = require('express')
 const app = express()
 const Gpio = require('onoff').Gpio //include onoff to interact with the GPIO
 
+const Lcd = require('lcd');
+const lcd = new Lcd({
+  rs: 2,
+  e: 44,
+  data: [66, 67, 68, 69],
+  cols: 16,
+  rows: 2
+});
+
+lcd.on('ready', () => {
+  setInterval(() => {
+    lcd.setCursor(0, 0);
+    lcd.print(new Date().toISOString().substring(11, 19), (err) => {
+      if (err) {
+        throw err;
+      }
+    });
+  }, 1000);
+});
+
+// If ctrl+c is hit, free resources and exit.
+process.on('SIGINT', () => {
+  lcd.close();
+  process.exit();
+});
+
 function makeLEDBlink(duration = 5000, interval = 250) {
   let LED = new Gpio(4, 'out') //use GPIO pin 4, and specify that it is output
   let blinkInterval = setInterval(blinkLED, 250) //run the blinkLED function every 250ms
